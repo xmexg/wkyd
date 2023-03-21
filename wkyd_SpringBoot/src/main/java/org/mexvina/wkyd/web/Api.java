23692c69969aa3bd31112e01e3bcedc00f200eb2
xmexg <javareturnclannadandairisnot0@gmail.com>
2023-03-21 19:33:33 +0800

    新增获取打卡点功能

--- wkyd_SpringBoot/src/main/java/org/mexvina/wkyd/web/Api.java
+++ wkyd_SpringBoot/src/main/java/org/mexvina/wkyd/web/Api.java
@@ -16,6 +16,7 @@ public class Api {
     @Autowired
     NetWork netWork;
 
+    //提交跑步记录
     @PostMapping("/run/addRunInfo")
     public String addRunInfo(@RequestParam String salt, @RequestParam String sign, @RequestBody Map<String, Object> runInfo) {
 
@@ -23,6 +24,17 @@ public class Api {
         String body = JSON.toJSONString(runInfo.get("BODY"));
 
         String res = netWork.sendPost2("http://sports.wfust.edu.cn/api/run/addRunInfo?salt="+salt+"&sign="+sign, head, body);
+        return res;
+    }
+
+    //获取4个打卡点
+    @PostMapping("/semester/queryPoint")
+    public String queryPoint(@RequestParam String salt, @RequestParam String sign, @RequestBody Map<String, Object> runInfo) {
+
+        Map<String, String> head = JSON.parseObject(JSON.toJSONString(runInfo.get("HEAD")), new TypeReference<Map<String, String>>() {});
+        String body = JSON.toJSONString(runInfo.get("BODY"));
+
+        String res = netWork.sendPost2("http://sports.wfust.edu.cn/api/semester/queryPoint?salt="+salt+"&sign="+sign, head, body);
         return res;
     }
 }
